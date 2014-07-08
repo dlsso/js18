cdc0db4d0d4da9b2ef6c5d719ae86038a11a66b7
dlsso <supertonesfan@hotmail.com>
2014-07-08 12:25:20 -0600

    Added lightbox with back button and single quote pulling

--- main.js
+++ main.js
@@ -16,10 +16,32 @@ $(document).on('ready', function() {
 		$('#quote-entry').val('')
 		$('#author-entry').val('')
 
-		$('#content').prepend('<h3>~ '+ author + '</h3>')
+		$('#content').prepend('<h3 class="author">~ '+ author + '</h3>')
 		$('#content').prepend('<p>' + quote + '</p>')
 		}
 
 	});
   
+
+	$(document).on('click', '.author', function() {
+
+		$('#lightbox').removeClass('hidden')
+		var author = $(this).text()
+		var quote = $(this).prev().text()
+
+
+		$('#author-content').prepend('<h3>'+ author + '</h3>')
+		$('#author-content').prepend('<p>' + quote + '</p>')
+
+
+	})
+
+	$(document).on('click', '#back', function() {
+
+		$('#lightbox').addClass('hidden')
+		$('#author-content').html('')
+
+	})
+
+
 });
