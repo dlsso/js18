72119b2690b59cd4066497edf4dda951cb226e5d
dlsso <supertonesfan@hotmail.com>
2014-07-08 11:09:07 -0600

    Added html template and basic quote adding

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+// var addQuote = $('<textarea class="quote-entry" placeholder="Enter your quote here"></textarea><br>');
+
+// var quote = $('<textarea class="quote-entry" placeholder="Enter your quote here"></textarea><br>');
+
+
+
+
 $(document).on('ready', function() {
+
+	$(document).on('click', '#submit', function() {
+
+		var quote = $('#quote-entry').val()
+		var author = $('#author-entry').val()
+		$('#quote-entry').val('')
+		$('#author-entry').val('')
+
+		$('#content').prepend('<h3>~ '+ author + '</h3>')
+		$('#content').prepend('<p>' + quote + '</p>')
+		
+
+	});
   
 });
